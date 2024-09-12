80748aa87ca609e0d636025006327d34bff890b0
En Tay <rush_x@yahoo.com>
2024-09-12 22:07:50 +0700

    fix download

--- download.php
+++ download.php
@@ -4,22 +4,22 @@ $arr = array();
 $d = dir($folder);
 $max = 0;
 while (false !== ($entry = $d->read())) {
-    if($entry!='.' && $entry!='..') {
+    if($entry!='.' && $entry!='..' && $entry!='README.md') {
         $line = file_get_contents($folder.$entry);
         $row = explode(";",$line);
         $arr[] = array(
             'nomor' => $entry,
-            'nama' => $row[0],
-            'jeniskelamin' => $row[2],
-            'telp' => $row[2],
-            'email' => $row[3],
-            'sosmed' => $row[4],
-            'whatsapp' => $row[5],
-            'gereja' => $row[6],
-            'keluarga' => $row[7],
-            'spanduk' => $row[8],
-            'lain' => $row[9],
-            'status' => $row[10]
+            'nama' => isset($row[0])?$row[0]:"",
+            'jeniskelamin' => isset($row[1])?$row[1]:"",
+            'telp' => isset($row[2])?$row[2]:"",
+            'email' => isset($row[3])?$row[3]:"",
+            'sosmed' => isset($row[4])?$row[4]:"",
+            'whatsapp' => isset($row[5])?$row[5]:"",
+            'gereja' => isset($row[6])?$row[6]:"",
+            'keluarga' => isset($row[7])?$row[7]:"",
+            'spanduk' => isset($row[8])?$row[8]:"",
+            'lain' => isset($row[9])?$row[9]:"",
+            'status' => isset($row[10])?$row[10]:""
         );
     }
 }
