4ccdb39968e7eb7935e7ad55701779dba5245892
En Tay <rush_x@yahoo.com>
2026-07-14 14:45:05 +0700

    Create tiket.php

---
+++ tiket.php
@@ -0,0 +1,189 @@
+<?php
+    $num = $_GET['num'];
+    $nomail = isset($_GET['nomail']);
+    $folder = "data/";
+
+    $line  = file_get_contents($folder.$num);
+    $row = explode(";", $line);
+
+    $nama = $row[0];
+    //$jeniskelamin = $row[1];
+    //$telp = $row[2];
+    $email = $row[3];
+
+?><!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Konser Edukasi Musik Klasik 2026 - Bandung</title>
+  <meta content="Konser Edukasi Musik Klasik 2026" name="description">
+  <meta content="musik klasik,billy kristanto,vivaldi,bach,buxtehude,massenet,of joy and pain,grii bandung,bandung,konser"
+    name="keywords">
+
+  <!-- WhatsApp Preview -->
+  <!-- ref: https://stackoverflow.com/questions/19778620/provide-an-image-for-whatsapp-link-sharing -->
+  <meta name="description" content="Sabtu, 15 Agustus 2026 ⌚ pk 17.00 📍 Aula Stefanus - GRII Bandung.">
+  <meta property="og:title" content="Tiket Konser Edukasi Musik Klasik 2026 - Bandung" />
+  <meta property="og:url" content="https://konseredukasi.com/of-joy-and-pain/" />
+  <meta property="og:description" content="Sabtu, 15 Agustus 2026 ⌚ pk 17.00 📍 Aula Stefanus - GRII Bandung.">
+  <meta property="og:image" content="https://konseredukasi.com/of-joy-and-pain/assets/img/logo-joy-square.jpeg">
+  <meta property="og:type" content="website" />
+  <meta property="og:locale" content="id_ID" />
+
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:title" content="Tiket Konser Edukasi Musik Klasik 2026 - Bandung">
+  <meta name="twitter:description" content="Sabtu, 15 Agustus 2026 ⌚ pk 17.00 📍 Aula Stefanus - GRII Bandung.">
+  <meta name="twitter:title" content="Tiket Konser Edukasi Musik Klasik 2026 - Bandung" />
+  <meta name="twitter:image" content="https://konseredukasi.com/of-joy-and-pain/assets/img/logo-joy-square.jpeg">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon-grii.png" rel="icon">
+  <link href="assets/img/apple-touch-icon-grii.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css?4" rel="stylesheet">
+
+</head>
+
+<body>
+  <header id="header" class="header fixed-top">
+    
+    <div class="branding d-flex align-items-center">
+      <div class="container position-relative d-flex align-items-center justify-content-between">
+        <a href="." class="logo d-flex align-items-center">
+          <!-- Uncomment the line below if you also wish to use an image logo -->
+          <img src="assets/img/logo-konser.png" alt="" style="height:100px">
+          <!-- <h1 class="sitename">Konser Edukasi</h1> -->
+        </a>
+
+        
+
+        <nav id="navmenu" class="navmenu">
+          <ul>
+            <li><a href="index.php#home" class="active">Home</a></li>
+            <li><a href="index.php#about">About</a></li>
+            <li><a href="index.php#profil">Profil</a></li>
+            <li><a href="index.php#lokasi">Lokasi</a></li>
+            <li><a href="index.php#cta">Hubungi Kami</a></li>
+            <li><a href="daftar.html" 
+                style="border:2px solid white; background:maroon; color:white; font-size:1.2em; 
+                        padding: 4px; width:100%; margin-bottom:6px; display:block;
+                        text-align:center; border-radius: 5px;
+                        ">
+                ✋ DAFTAR
+            </a></li>
+          </ul>
+          <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+      </div>
+    </div> <!-- END: div branding -->
+
+  </header>
+
+  <main id="main">
+    <section id="form" style="padding-top:10px; max-width: 500px;" class="mx-auto">
+      <div>
+        <p>Terimakasih <b><?php echo $nama; ?></b></p>
+        <p>Anda telah terdaftar dalam <i>Konser Edukasi Musik Klasik 2026</i> dengan nomer :</p>
+        <div style="font-size:5em">
+          <?php echo $num; ?>
+        </div>
+        <p>
+        <p>
+          Mohon bisa hadir di acara 30 menit sebelum konser dimulai.<br>
+          Alamat GRII Bandung : <a href="https://maps.app.goo.gl/LonewXLZtW8ToEng8">Jl. Moch. Toha 229</a> <br>
+          Informasi lebih lengkap : <a href="https://konseredukasi.com/gloria/">konseredukasi.com</a><br>
+          Kontak kami : 0851-0507-1880 <a href="tel:+6285105071880">Telp</a> / <a href="https://wa.me/6285105071880">WhatsApp</a>
+        </p>
+        <br>
+        <?php if($nomail) { ?>
+        <p>          
+          Gunakan tangkapan layar/screenshot halaman ini untuk digunakan pada saat kehadiran.
+        </p>
+        <?php } else { ?>
+
+          <p>
+            Bukti pendaftaran sudah dikirim lewat email ke <strong><?php echo $email; ?></strong>.<br>
+            Check <b>SPAM FOLDER</b> untuk kemungkinan tiket tertanda sebagai <i>spam</i>.
+          </p>
+          <p>
+            Untuk kemudahan, Anda juga bisa gunakan tangkapan layar/screenshot halaman ini untuk digunakan pada saat kehadiran.
+          </p>
+        <?php } ?>
+      </div>
+    </section>
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-info">
+      <!--h4>STEMI: Stephen Tong Evengelistic Ministry</h4-->
+      <!--p style="max-width:500px; font-size:0.9em"><a href="https://stemi.org">STEMI</a> merupakan sebuah organisasi yang
+        berfokus pada Penginjilan dan Mandat Budaya. STEMI didirikan oleh Pdt. Dr. Stephen Tong pada tahun 1978 bersama
+        dengan Dr. Jahja Ling. Pelayanan STEMI dimulai dengan mengadakan setiap tahun di berbagai kota di dunia. Hingga
+        kini, STEMI terus melayani ratusan ribu hingga jutaan jiwa dari berbagai daerah dan luar negeri setiap tahunnya.
+        Pelayanan STEMI bergerak berdasarkan semangat dan prinsip Gerakan Reformed Injili.</p-->
+      <div class="social-links mt-3 text-center">
+        <a href="https://griibandung.org"><i class="bx bx-globe"></i></a>
+        <a href="https://www.facebook.com/grii.bandung" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="https://www.instagram.com/griibdg" class="instagram"><i class="bx bxl-instagram"></i></a>
+      </div>
+
+    </div>
+
+    <div class="container">
+
+      <div class="copyright">
+        &copy; Copyright <strong><span>Day</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX lokasi form: https://bootstrapmade.com/day-multipurpose-html-template-for-free/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        <a href='http://www.freevisitorcounters.com'>www.freevisitorcounters.com</a> <script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=b0b6012a43339d0c93a411ff071202537a39070c'></script>
+        <script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/1222643/t/4"></script>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+
+
+</body>
+
+</html>
